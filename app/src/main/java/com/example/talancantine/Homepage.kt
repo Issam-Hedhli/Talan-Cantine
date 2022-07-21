ccb6da4a4c995cf6e6ed337364332294f2a5d261
Issam <issam.hedhli@ensi-uma.tn>
2022-07-21 16:03:14 +0100

    yey

---
+++ app/src/main/java/com/example/talancantine/Homepage.kt
@@ -0,0 +1,26 @@
+package com.example.talancantine
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.talancantine.databinding.ActivityHomepageBinding
+import com.example.talancantine.databinding.ActivitySignInBinding
+
+class Homepage : AppCompatActivity() {
+    private lateinit var binding: ActivityHomepageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signIn.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
+
+
+        binding.signUp.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+    }
+}
